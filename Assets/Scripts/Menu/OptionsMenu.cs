a20ea9fbd9d9388418a27270a7c8cee324f81866
Veliki5382 <kuzmanov03@gmail.com>
2025-02-02 22:00:30 +0100

    Options Menu

--- Assets/Scripts/Menu/OptionsMenu.cs
+++ Assets/Scripts/Menu/OptionsMenu.cs
@@ -1,13 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioMixer audioMixer;
+	public Dropdown resolutionDropdown;
+	Resolution[] _resolutions;
+	void Start()
     {
-        gameObject.SetActive(false);
+		resolutionDropdown.ClearOptions();
+		List<string> options = new List<string>();
+		_resolutions = Screen.resolutions;
+		
+		int currentResolutionIndex = 0;
+		for (int i = 0; i < _resolutions.Length; i++)
+		{
+			string option = _resolutions[i].width + " x " + _resolutions[i].height;
+			options.Add(option);
+
+			if (_resolutions[i].width == Screen.currentResolution.width &&
+				_resolutions[i].height == Screen.currentResolution.height)
+			{
+				currentResolutionIndex = i;
+			}
+		}
+		resolutionDropdown.AddOptions(options);
+		resolutionDropdown.value = currentResolutionIndex;
+		resolutionDropdown.RefreshShownValue();
+
+		gameObject.SetActive(false);
 	}
 
+	public void SetResolution(int resolutionIndex)
+	{
+		Screen.SetResolution(_resolutions[resolutionIndex].width, _resolutions[resolutionIndex].height, Screen.fullScreen);
+	}
+
+	public void SetFullscreen(bool state)
+	{
+		Screen.fullScreen = state;
+	}
+
+	public void SetVolume(float volume)
+	{
+		audioMixer.SetFloat("volume", volume);
+	}
 }
