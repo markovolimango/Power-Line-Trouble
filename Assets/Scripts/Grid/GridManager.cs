1f2779239099488d77eeae13c6f7f96a35f1b558
marko <maremare005@gmail.com>
2025-02-02 13:13:07 +0100

    bar

--- Assets/Scripts/Grid/GridManager.cs
+++ Assets/Scripts/Grid/GridManager.cs
@@ -40,8 +40,6 @@ namespace Grid
             for (var j = 0; j < n - 1; j++)
             {
                 var branch = Instantiate(horizontalBranchPrefab, NodePositions[i, j], transform.rotation, transform);
-                branch.transform.position =
-                    new Vector3(branch.transform.position.x, branch.transform.position.y, m - i);
                 HorizontalBranches[i, j] = branch.GetComponent<HorizontalBranch>();
                 HorizontalBranches[i, j].SetEdges(NodePositions[i, j], NodePositions[i, j + 1]);
             }
@@ -51,8 +49,6 @@ namespace Grid
             for (var i = 0; i < m - 1; i++)
             {
                 var branch = Instantiate(verticalBranchPrefab, NodePositions[i, j], transform.rotation, transform);
-                branch.transform.position =
-                    new Vector3(branch.transform.position.x, branch.transform.position.y, m - i);
                 VerticalBranches[i, j] = branch.GetComponent<VerticalBranch>();
                 VerticalBranches[i, j].SetEdges(NodePositions[i, j], NodePositions[i + 1, j]);
             }
